a51ad5bab6256a6125bb73c18ff7081a7d5e709b
drgfragkos <gfragkos@gmail.com>
2015-05-19 20:24:52 +0100

    made grep non case sensitive

--- gHybridWebSearch.sh
+++ gHybridWebSearch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ## gHybridWebSearch version 0.2 - Quick Hybrid Web Search for Old, Backup and Unreferenced ##
-## Files for Sensitive Information based on a custom dictionary (c)gfragkos 2012          ##
+## Files for Sensitive Information based on a custom dictionalry (c)gfragkos 2012          ##
 ## You may modify, reuse and distribute the code freely as long as it is referenced back   ##
-## to the author using the following line: ..based on gHybridWebSearch.sh by @drgfragkos      ##
+## to the author using the following line: ..based on gHybridWebSearch by @drgfragkos      ##
 
 if [ "$1" == "" ]; then
 echo -ne "You need to pass a URL as an argument to work.\n  Usage: ./${0##*/} www.example.com\n"
@@ -23,9 +23,9 @@ echo -e "GET /$line HTTP/1.0\nHost: $server\n" | netcat $server $port | head -1
 
 done < "hybridWebSearch.dic" | tee .log.dat
 
-cat .log.dat | grep "200 OK" > output-200.txt
+cat .log.dat | grep -i "200 OK" > output-200.txt
 sleep 0.10
-cat .log.dat | grep -v "404 Not Found" > output-ex404.txt
+cat .log.dat | grep -v -i "404 Not Found" > output-ex404.txt
 
 #rm .log.dat   ## in case the main log file in not needed to be kept for further searches
 
